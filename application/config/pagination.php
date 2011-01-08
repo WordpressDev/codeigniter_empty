45746287678f0c412d4dd190f1305655c69dbe44
accent-interactive <joost@accentinteractive.nl>
2011-01-08 09:45:27 +0100

    Added pagination config

---
+++ application/config/pagination.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Status: final
+ * 
+ * General config settings for paginaton
+ * 
+ * LICENSE AND COPYRIGHT
+ * 
+ * The intellectual property rights and copyright to this product belong to 
+ * Accent Interactive, The Netherlands. None of this code may be edited, shared,
+ * sold, copied or reproduced without Accent Interactive's prior written consent.
+ * 
+ * Anybody who edits this product, or adds to it, whether or not in commision 
+ * for Accent Interactive, automatically and exclusively transfers full 
+ * ownership of all and any intellectual rights and copyrights for these 
+ * additions or alterations to Accent Interactive.
+ * 
+ * If the editor makes use of external code of contractors who can claim any 
+ * kind of intellectual ownership regarding the added or altered code in this 
+ * document, then the editor will secure that these rights will comply fully to
+ * the clause mentioned above.
+ * 
+ * Accent Interactive's clients may recieve a non-unique, non-transferable license
+ * to use this product.
+ * 
+ * @category Accent_Interactive
+ * @package Core
+ * @subpackage Config
+ * @version 1.0
+ * @author Joost van Veen
+ * @copyright Accent Interactive
+ */
+
+$config['full_tag_open'] = '<div class="pagination">';
+$config['full_tag_close'] = '</div>';
+
+$config['num_links'] = 20;
+$config['first_link'] = '&laquo;';
+$config['last_link'] = '&raquo;';
