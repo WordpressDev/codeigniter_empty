80f0d81f22827a0301bef355789656b75b2da8ee
accent-interactive <joost@accentinteractive.nl>
2011-01-08 12:02:03 +0100

    Removed obsolete copyright notices

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -6,23 +6,6 @@
  * 
  * LICENSE AND COPYRIGHT
  * 
- * The intellectual property rights and copyright to this product belong to 
- * Accent Interactive, The Netherlands. None of this code may be edited, shared,
- * sold, copied or reproduced without Accent Interactive's prior written consent.
- * 
- * Anybody who edits this product, or adds to it, whether or not in commision 
- * for Accent Interactive, automatically and exclusively transfers full 
- * ownership of all and any intellectual rights and copyrights for these 
- * additions or alterations to Accent Interactive.
- * 
- * If the editor makes use of external code of contractors who can claim any 
- * kind of intellectual ownership regarding the added or altered code in this 
- * document, then the editor will secure that these rights will comply fully to
- * the clause mentioned above.
- * 
- * Accent Interactive's clients may recieve a non-unique, non-transferable license
- * to use this product.
- * 
  * @category Accent_Interactive
  * @package Core
  * @subpackage Config
